dbf0bdccbb90a791c98a0d1a9478cf794f6d9b50
LuanaRSon <luhhsonaglio@gmail.com>
2022-03-11 16:42:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# What time is it?
+
+<img src="gif-webpage.gif" alt="três imagens mostrando as alterações de cor de fundo e imagem conforme a hora mostrada.">
+
+> Uma página web que mostra a hora atual (e atualiza conforme o tempo passa) e, conforme o horário, altera a cor de fundo e a imagem.
+
+
+
+## 🤝 Colaboradores
+
+Agradecemos às seguintes pessoas que contribuíram para este projeto:
+
+<table>
+  <tr align="center">
+    <td>
+      <a href="#">
+        <img src="https://avatars.githubusercontent.com/u/88160281?v=4" width="100px;" alt="Foto de Luana no GitHub"/><br>
+        <sub>
+          <b>Luana</b>
+        </sub>
+      </a>
+    </td>   
+  </tr>
+</table>
+
+
+
+
---
+++ script.js
@@ -0,0 +1,57 @@
+const images = [
+{
+  altText: "Imagem do céu da manhã.",
+  srcImg: "./assets/morning.jpg"
+},
+{
+  altText: "Imagem do céu da tarde.",
+  srcImg: "./assets/afternoon.jpg"
+},
+{
+  altText: "Imagem do céu da noite.",
+  srcImg: "./assets/night.jpg"
+}];
+
+function getTime() {  
+  const message = document.getElementById("hour-message"); 
+
+  const data = new Date();
+  let hour = data.getHours();
+  let minutes = data.getMinutes();
+
+  if (hour < 10 && minutes < 10) {
+    message.innerHTML = `Agora são 0${hour}:0${minutes} horas.`    
+  } else if (hour < 10 && minutes > 10) {
+    message.innerHTML = `Agora são 0${hour}:${minutes} horas.`    
+  } else if (hour > 10 && minutes < 10) {
+    message.innerHTML = `Agora são ${hour}:0${minutes} horas.`  
+  } else {
+    message.innerHTML = `Agora são ${hour}:${minutes} horas.`
+  }  
+  changeLayout(hour);
+  setTimeout("getTime()",500);
+}
+
+function changeLayout(hour) {  
+  const img = document.getElementById("main-img");
+  const body = document.getElementsByTagName("body")[0];    
+
+  if (hour >= 0 && hour < 12) {
+    img.src = images[0].srcImg;
+    img.alt = images[0].altText;
+    document.body.style.background = "linear-gradient(rgba(97, 140, 241, 0.897) 35%, rgb(53, 87, 165)";      
+
+  } else if (hour >= 12 && hour <= 18) {
+    img.src = images[1].srcImg;
+    img.alt = images[1].altText;   
+    document.body.style.background = "linear-gradient(#f1ae8cd8 35%, #e07139)";     
+
+  } else {    
+    img.src = images[2].srcImg;
+    img.alt = images[2].altText;
+    document.body.style.background = "linear-gradient(#314B50 35%, #2f3538)";    
+
+  }
+};
+
+
