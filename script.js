2d688273c6f86d1910b86905e1afe899705118b2
LuanaRSon <luhhsonaglio@gmail.com>
2022-03-12 18:51:11 -0300

    Adjusted the time for the background change

--- script.js
+++ script.js
@@ -36,12 +36,12 @@ function changeLayout(hour) {
   const img = document.getElementById("main-img");
   const body = document.getElementsByTagName("body")[0];    
 
-  if (hour >= 0 && hour < 12) {
+  if (hour >= 6 && hour < 12) {
     img.src = images[0].srcImg;
     img.alt = images[0].altText;
     document.body.style.background = "linear-gradient(rgba(97, 140, 241, 0.897) 35%, rgb(53, 87, 165)";      
 
-  } else if (hour >= 12 && hour <= 18) {
+  } else if (hour >= 12 && hour < 18) {
     img.src = images[1].srcImg;
     img.alt = images[1].altText;   
     document.body.style.background = "linear-gradient(#f1ae8cd8 35%, #e07139)";     
