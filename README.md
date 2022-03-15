62f87067cf2eaa1ca1183088ef4480ef066c68d9
LuanaRSon <luhhsonaglio@gmail.com>
2022-03-15 15:49:42 -0300

    time input, select and a button added for alarm clock feature

--- README.md
+++ README.md
@@ -2,9 +2,21 @@
 
 <img src="gif-webpage.gif" alt="três imagens mostrando as alterações de cor de fundo e imagem conforme a hora mostrada.">
 
-> Uma página web que mostra a hora atual (e atualiza conforme o tempo passa) e, conforme o horário, altera a cor de fundo e a imagem.
+> Uma página web que mostra a hora atual (e atualiza conforme o tempo passa) e, conforme o horário, altera a cor de fundo e a imagem. A função de despertador está desativada pois está em desenvolvimento.
 
 <b>A web page pode ser visualizada [aqui](https://whatimeisit.netlify.app/).</b>
+
+
+
+### Ajustes e melhorias
+
+O projeto ainda está em desenvolvimento e as próximas atualizações serão voltadas nas seguintes tarefas:
+
+- [ ] Acrescentar função de despertador (o botão "ativar alarme" está desabilitado);
+
+- [ ] Conversor de horas.
+
+  
 
 ## 🤝 Colaboradores
 
--- script.js
+++ script.js
@@ -12,6 +12,8 @@ const images = [
   srcImg: "./assets/night.jpg"
 }];
 
+const btnAlarm = document.getElementsByClassName("btn-alarm")[0];
+
 function getTime() {  
   const message = document.getElementById("hour-message"); 
 
@@ -19,39 +21,54 @@ function getTime() {
   let hour = data.getHours();
   let minutes = data.getMinutes();
 
-  if (hour < 10 && minutes < 10) {
-    message.innerHTML = `Agora são 0${hour}:0${minutes} horas.`    
-  } else if (hour < 10 && minutes > 10) {
-    message.innerHTML = `Agora são 0${hour}:${minutes} horas.`    
-  } else if (hour > 10 && minutes < 10) {
-    message.innerHTML = `Agora são ${hour}:0${minutes} horas.`  
-  } else {
-    message.innerHTML = `Agora são ${hour}:${minutes} horas.`
-  }  
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  message.innerHTML = `Agora são ${hour}:${minutes} horas.` 
   changeLayout(hour);
   setTimeout("getTime()",500);
 }
 
 function changeLayout(hour) {  
-  const img = document.getElementById("main-img");
-  const body = document.getElementsByTagName("body")[0];    
+  const img = document.getElementById("main-img");    
 
   if (hour >= 6 && hour < 12) {
     img.src = images[0].srcImg;
     img.alt = images[0].altText;
-    document.body.style.background = "linear-gradient(rgba(97, 140, 241, 0.897) 35%, rgb(53, 87, 165)";      
+    document.body.style.background = "linear-gradient(rgba(97, 140, 241, 0.897) 35%, rgb(53, 87, 165)";
+    btnAlarm.style.background = "rgba(97, 140, 241, 0.897)";   
 
   } else if (hour >= 12 && hour < 18) {
     img.src = images[1].srcImg;
     img.alt = images[1].altText;   
-    document.body.style.background = "linear-gradient(#f1ae8cd8 35%, #e07139)";     
+    document.body.style.background = "linear-gradient(#f1ae8cd8 35%, #e07139)";   
+    btnAlarm.style.background = "#f1ae8cd8";  
 
   } else {    
     img.src = images[2].srcImg;
     img.alt = images[2].altText;
-    document.body.style.background = "linear-gradient(#314B50 35%, #2f3538)";    
+    document.body.style.background = "linear-gradient(#314B50 35%, #2f3538)"; 
+    btnAlarm.style.background = "#314B50";   
 
   }
 };
 
+// EM DESENVOLVIMENTO
+// function setAlarm(hour, minutes) {
+//   const alarmTime = document.getElementsByClassName("input-time")[0].value;
+//   const song = document.getElementsByClassName("select-song")[0].value;  
+// };
 
+// btnAlarm.addEventListener("click", setAlarm);
+
+// EM DESENVOLVIMENTO
+// function showTime() {
+//   const message = document.getElementById("hour-message"); 
+
+//   setTimeout("showTime()",500);
+// }
